a56dac49017063b84c3ca45b56247175baea31f0
Irvig Krehan <irvigkrehan@hotmail.com>
2022-03-29 15:37:13 -0300

    refactor: pass maze size when calling

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import MazeSolver from './solver';
 import MazeBuilder from './maze';
 
-const maze = new MazeBuilder();
+const {columns, rows} = process.stdout;
+const maze = new MazeBuilder({
+  width: Math.floor(columns/4)-1,
+  height: Math.floor(rows/2)-1,
+});
+
 const solver = new MazeSolver(maze, true);
 solver.solve();
